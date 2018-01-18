37dd415eec041bfd699370dd4f3a69b0d45c9bd2
Leonardo Acevedo <leonardo.acevedo@mediamonks.com>
2018-01-18 14:32:29 -0300

    Android Application object: initialize mReactNativeHost after onCreate() has been called, which removes the need for invoking SoLoader directly.

--- android/app/src/main/java/com/example/MainApplication.java
+++ android/app/src/main/java/com/example/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 import com.mediamonks.rnnativenavigation.bridge.RNNativeNavigationPackage;
 import com.mediamonks.rnnativenavigation.data.Node;
 
@@ -16,23 +15,7 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            HashMap<String, Class<? extends Node>> hashMap = new HashMap<>();
-            hashMap.put(ExampleNode.JS_NAME, ExampleNode.class);
-
-            return Arrays.asList(
-                    new RNNativeNavigationPackage(hashMap),
-                    new MainReactPackage()
-            );
-        }
-    };
+    private ReactNativeHost mReactNativeHost;
 
     @Override
     public ReactNativeHost getReactNativeHost() {
@@ -42,6 +25,22 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+        mReactNativeHost = new ReactNativeHost(this) {
+            @Override
+            public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                HashMap<String, Class<? extends Node>> hashMap = new HashMap<>();
+                hashMap.put(ExampleNode.JS_NAME, ExampleNode.class);
+
+                return Arrays.asList(
+                        new RNNativeNavigationPackage(hashMap),
+                        new MainReactPackage()
+                );
+            }
+        };
     }
 }
